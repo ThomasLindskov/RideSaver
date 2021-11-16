593adb655605e0a2a583373ba0f5b42bcaaaa492
thch@it-kartellet.dk <thch19ab@student.cbs.dk>
2021-11-16 12:46:29 +0100

    Uppercase n

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import TabNavigator from './Components/navigation/TabNavigator';
+import TabNavigator from './Components/Navigation/TabNavigator';
 
 export default function App() {
   const Stack = createNativeStackNavigator();
